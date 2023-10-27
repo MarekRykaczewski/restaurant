856fddd2e11c70723352dd79088cfe42d41674a8
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-10-27 13:56:53 +0200

    style footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-4 text-red-500 flex items-center justify-between">
+      <Link href={"/"} className="font-bold text-xl">
+        {" "}
+        MASSIMO{" "}
+      </Link>
+      <p> ALL RIGHT RESERVED</p>
+    </div>
+  );
 };
 
 export default Footer;
