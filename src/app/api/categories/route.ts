c45e404081397f9b7961b8f492f2b7a86b15013c
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-10-31 13:47:19 +0100

    add fetch all categories request

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,9 +1,18 @@
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-export const GET = () => {
-  return new NextResponse("HELLO", { status: 200 });
-};
+const prisma = new PrismaClient();
 
-export const POST = () => {
-  return new NextResponse("HELLO", { status: 200 });
+// FETCH ALL CATEGORIES
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(JSON.stringify(categories), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
 };
